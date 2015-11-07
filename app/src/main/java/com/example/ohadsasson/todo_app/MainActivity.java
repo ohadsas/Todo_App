69f839b029961c5a8cccff1a17bf5adba15726af
ohadsasson <ohad@panda-os.com>
2015-11-07 16:38:44 +0200

    second exercise

--- app/src/main/java/com/example/ohadsasson/todo_app/MainActivity.java
+++ app/src/main/java/com/example/ohadsasson/todo_app/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.ohadsasson.todo_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lv =(ListView) findViewById(R.id.listView);
+        Button button = (Button) findViewById(R.id.button);
+        }
+
+    void onDoneButtonClick(View view) {
+        View v = (View) view.getParent();
+        TextView taskTextView = (TextView) v.findViewById(R.id.task);
+        String task = taskTextView.getText().toString();
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.button:
+                Log.d("MainActivity", "Add a new task");
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
 }
